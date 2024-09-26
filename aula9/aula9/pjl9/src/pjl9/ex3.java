e2c61a2db289f5411815951ea1ae2419bf0a9e99
Vinicius <vinicius2007henrique@gmail.com>
2024-09-26 09:06:29 -0300

    Add files via upload

---
+++ aula9/aula9/pjl9/src/pjl9/ex3.java
@@ -0,0 +1,16 @@
+package pjl9;
+public class ex3 {
+	
+	public static void main(String[] args) {
+		int i  =1;
+		int soma =0;
+		
+		System.out.println("o valor da soma dos numeros é:");
+		
+		while (i <=100) {
+			soma +=i;
+			i++;
+			System.out.println( + soma );
+		}
+	}
+}
